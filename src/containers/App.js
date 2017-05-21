7a8f66f1eb23863fc0caa11db4f48639528f9c68
velopert <public.velopert@gmail.com>
2017-05-21 21:25:43 +0900

    메모 불러오기

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,18 +2,37 @@ import React, { Component } from 'react';
 import Header from 'components/Header';
 import Layout from 'components/Layout';
 import WriteMemo from './WriteMemo';
+import MemoListContainer from './MemoListContainer';
+
+import * as memoActions from 'modules/memo';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
 
 class App extends Component {
+
+    componentDidMount() {
+        const { MemoActions } = this.props;
+        // 초기 메모 로딩
+        MemoActions.getInitialMemo();
+    }
+    
     render() {
         return (
             <Layout>
                 <Header/>
                 <Layout.Main>
                     <WriteMemo/>
+                    <MemoListContainer/>
                 </Layout.Main>
             </Layout>
         );
     }
 }
 
-export default App;
+export default connect(
+    (state) => ({}), // 현재는 비어있는 객체를 반환합니다
+    (dispatch) => ({
+        MemoActions: bindActionCreators(memoActions, dispatch)
+    })
+)(App);
